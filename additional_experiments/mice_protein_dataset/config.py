e71d031db44a5d2dcd1a9cdd7ab65d960253f365
eirihoyh <eirik.hoy@gmail.com>
2025-07-02 17:13:35 +0200

    updated mice protein experiments, sparser model and better global plotting

--- additional_experiments/mice_protein_dataset/config.py
+++ additional_experiments/mice_protein_dataset/config.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 config = {}
-config['num_epochs' ] = 1000  
-config['lr'] = .025
+config['num_epochs' ] = 2000  
+config['lr'] = .01
 config['post_train_epochs'] = 0  # If post training, then only MPM will be trained
 config['patience'] = 100000
 config['n_nets'] = 1  # Number of different networks to run for one problem
@@ -12,9 +12,9 @@ config['num_transforms'] = 2  # For normalizing flows
 config['class_problem'] = True  # If classification problem or not
 config['inclusion_prob_prior'] = 0.05
 config['std_prior'] = 25.
-config['lower_init_lambda'] = 3
-config['upper_init_lambda'] = 5
-config['high_init_covariate_prob'] = False # If true, the inital covariate probs for the covariates will be set to lambda=5
+config['lower_init_lambda'] = 0
+config['upper_init_lambda'] = 3
+config['high_init_covariate_prob'] = True # If true, the inital covariate probs for the covariates will be set to lambda=5
 # For the linear and non-linear problem
 config['n_samples'] = 4*10**4
 config['non_lin'] = False  # Wanting to create a non-linear or linear dataset 
